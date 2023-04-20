bb8e8b5608bbf3f0add7c6478a25e0ee8b984cfb
HomieSubodh <golang602@gmail.com>
2023-04-20 10:17:50 +0600

    search page header added

--- components/SearchPageHeader/SearchPageHeader.jsx
+++ components/SearchPageHeader/SearchPageHeader.jsx
@@ -1,14 +1,39 @@
 import { useRouter } from 'next/router';
 import React from 'react';
+import Style from './SearchPageHeader.module.css';
+import { VscSearch } from 'react-icons/vsc';
 
 const SearchPageHeader = () => {
     const router = useRouter();
 
     return (
-        <header>
-            <h1
-                onClick={() => router.push("/")}
-                className=" mb-4 text-transparent font-extrabold text-2xl bg-clip-text bg-gradient-to-r from-orange-500 to-yellow-500 cursor-pointer">GPT Search</h1>
+        <header className='mb-5'>
+            <div className='flex max-sm:flex-col gap-4 items-center mt-5 h-30'>
+                <h1
+                    onClick={() => router.push("/")}
+                    className="pt-5 mb-4 text-transparent font-extrabold text-4xl bg-clip-text bg-gradient-to-r from-orange-500 to-yellow-500 cursor-pointer">
+                    GPT Search
+                </h1>
+
+                <div className='lg:justify-center  flex'>
+                    <div className={`h-30 max-sm:w-[350px] w-[400px] flex justify-between pl-3 rounded-lg gap-3 bg-white ${Style.inputBox}`}>
+
+                        {/* input field */}
+                        <input
+                            type="text"
+                            // ref={searchInputRef}
+                            placeholder='What are you looking for?'
+                            className='pl-1 pb-1 max-sm:w-full w-[300px] text-xl outline-none focus:outline-none border-black bg-inherit'
+                        />
+                        <button
+                            // onClick={search}
+                            className='bg-gradient-to-r from-orange-500 to-yellow-500 p-1 h-[50px] w-[50px] flex justify-center items-center cursor-pointer rounded-r-lg'>
+                            <VscSearch className='text-3xl text-white' />
+                        </button>
+                    </div>
+                </div>
+
+            </div>
         </header>
     );
 };

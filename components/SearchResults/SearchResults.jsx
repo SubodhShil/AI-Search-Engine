5171aa579a6f8590da10a5f964a735332092d77f
HomieSubodh <golang602@gmail.com>
2023-04-21 11:08:04 +0600

    chat gpt part added

--- components/SearchResults/SearchResults.jsx
+++ components/SearchResults/SearchResults.jsx
@@ -3,18 +3,52 @@ import Style from './SearchResults.module.css';
 
 const SearchResults = ({ results }) => {
     return (
-        <div className='flex items-center'>
-            <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
-            <a href="https://lordicon.com/"></a>
-            <lord-icon
-                src="https://cdn.lordicon.com/dfxesbyu.json"
-                trigger="loop"
-                style={{ width: '50px', height: '50px' }}
-                loop>
-            </lord-icon>
-            <h1 className={`font-[500] w-fit p-1 ${Style.highlight}`}>
-                Showing you {results?.searchInformation?.formattedTotalResults} results
-            </h1>
+        <div className='overflow-hidden'>
+
+            {/* Result count header part */}
+            <div className='flex items-center'>
+                <div className='mr-4'>
+                    <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+                    <lord-icon
+                        src="https://cdn.lordicon.com/dfxesbyu.json"
+                        trigger="loop"
+                        style={{ width: '50px', height: '50px' }}
+                        loop>
+                    </lord-icon>
+                </div>
+                <h1 className={`font-[500] w-fit p-1 ${Style.highlight}`}>
+                    Showing you {results?.searchInformation?.formattedTotalResults} results
+                </h1>
+            </div>
+
+            {/* Showing the result in the page */}
+            <div className='mt-4 grid max-sm:grid-cols-1 grid-cols-3 items-center p-2'>
+                {results.items.map((result) => (
+                    <div className='border m-2 col-span-2' key={result.link}>
+                        <a
+                            href={result.link}
+                            className=''
+                        >
+                            {result.htmlFormattedUrl}
+                        </a>
+                    </div>
+
+                ))}
+
+                {/* GPT Chat Part */}
+
+                <div className='col-span-1 bg-[#F9F9F9] border p-2 flex items-center'>
+                    <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+                    <lord-icon
+                        src="https://cdn.lordicon.com/mjmrmyzg.json"
+                        trigger="loop"
+                        style={{ width: '75px', height: '75px' }}
+                        loop>
+                    </lord-icon>
+                    <h1>Chat With The AI</h1>
+                </div>
+            </div>
+
         </div>
     );
 };

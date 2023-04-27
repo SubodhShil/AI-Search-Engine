62e523803b75bc9360e84e1a9974f9906c906aad
HomieSubodh <golang602@gmail.com>
2023-04-27 12:15:37 +0600

    Chat gpt part fully designed

--- components/SearchResults/SearchResults.jsx
+++ components/SearchResults/SearchResults.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Style from './SearchResults.module.css';
+import PaginationButtons from '../PaginationButtons/PaginationButtons';
+import { GrSend } from 'react-icons/gr';
 
 const SearchResults = ({ results }) => {
     return (
@@ -23,9 +25,9 @@ const SearchResults = ({ results }) => {
             </div>
 
             {/* Showing the result in the page */}
-            <div className='mt-4 grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 items-center p-2'>
+            <div className='mt-4 grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 p-2'>
 
-                <div className=' col-span-2 p-1'>
+                <div className='col-span-2 p-1'>
                     {results.items.map((result) => (
                         <div className='border rounded-md pl-3 m-2 bg-[ghostwhite] group pt-3' key={result.link}>
 
@@ -51,18 +53,38 @@ const SearchResults = ({ results }) => {
 
                 {/* GPT Chat Part */}
 
-                <div className='col-span-1 bg-[#191825] rounded-lg  p-2 flex items-center text-white'>
-                    <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
-                    <lord-icon
-                        src="https://cdn.lordicon.com/mjmrmyzg.json"
-                        trigger="loop"
-                        style={{ width: '75px', height: '75px' }}
-                        loop>
-                    </lord-icon>
-                    <h1>Chat With The AI</h1>
+                <div className='backdrop-blur-sm col-span-1 bg-gradient-to-r from-green-50 to-cyan-100 rounded-lg p-3 flex flex-col text-white h-fit mt-3 max-sm:mb-5 shadow-lg shadow-gray-700/200'>
+                    <div className='flex items-center'>
+                        <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/mjmrmyzg.json"
+                            trigger="loop"
+                            style={{ width: '75px', height: '75px' }}
+                            loop>
+                        </lord-icon>
+                        <h1 className='text-black text-xl'>Chat With The AI</h1>
+                    </div>
+
+                    {/* Result prompts */}
+                    <div className='p-2 rounded-lg bg-gradient-to-r from-orange-100 to-yellow-100 text-black'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ab, facilis magnam iure cum ducimus sed omnis quaerat eligendi eos officia explicabo deleniti fugiat qui esse nemo fuga quae, numquam repudiandae accusamus consequatur animi sapiente? Officia, facere necessitatibus ut distinctio perspiciatis nemo, dolores ipsam dolore, aut unde iure animi enim natus. Temporibus nisi omnis ducimus tempora excepturi eos quis quos magnam nihil, incidunt suscipit architecto iure quia consequatur, voluptates, eum vitae nobis voluptas hic repudiandae ex? Dolores, ullam harum! Saepe minima ipsa et. Delectus laboriosam numquam velit obcaecati harum molestias dolore. Sed quis, voluptas ipsum architecto distinctio magni a nulla.
+                    </div>
+
+                    {/* search bar */}
+                    <div className='flex items-center justify-between h-full p-1 text-black'>
+                        <div className='border-2 border-blue-700 rounded-lg mt-4 w-[90%] '>
+                            <input type="text" className='bg-inherit w-full pl-2 p-1' placeholder='Type your prompt' />
+                        </div>
+
+                        <div className='text-white rounded-xl bg-cyan-400 shadow-lg shadow-cyan-600/100 h-10 w-10 mt-3 cursor-pointer flex justify-center items-center'>
+                            <GrSend className='text-2xl w-full' color='white' />
+                        </div>
+                    </div>
                 </div>
             </div>
 
+
+            <PaginationButtons />
         </div>
     );
 };

518e4d6f20682b619ae22416a884eb835f4e1e0d
HomieSubodh <golang602@gmail.com>
2023-04-27 11:19:38 +0600

    chat gpt grid added

--- components/SearchResults/SearchResults.jsx
+++ components/SearchResults/SearchResults.jsx
@@ -3,10 +3,11 @@ import Style from './SearchResults.module.css';
 
 const SearchResults = ({ results }) => {
     return (
-        <div className='overflow-hidden'>
+        <div className=''>
 
             {/* Result count header part */}
             <div className='flex items-center'>
+
                 <div className='mr-4'>
                     <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
                     <lord-icon
@@ -22,18 +23,31 @@ const SearchResults = ({ results }) => {
             </div>
 
             {/* Showing the result in the page */}
-            <div className='mt-4 grid max-sm:grid-cols-1 grid-cols-3 items-center p-2'>
-                {results.items.map((result) => (
-                    <div className='border m-2 col-span-2' key={result.link}>
-                        <a
-                            href={result.link}
-                            className=''
-                        >
-                            {result.htmlFormattedUrl}
-                        </a>
-                    </div>
+            <div className='mt-4 grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 items-center p-2'>
 
-                ))}
+                <div className=' col-span-2 p-1'>
+                    {results.items.map((result) => (
+                        <div className='border rounded-md pl-3 m-2 bg-[ghostwhite] group pt-3' key={result.link}>
+
+                            {/* URL */}
+                            <a
+                                href={result.link}
+                                className={`text-[13px] p-1 bg-gray-400 text-white rounded-md`}
+                            >
+                                {result.link}
+                            </a>
+
+                            {/* Clickable link text */}
+                            <a href="">
+                                <h2 className='text-blue-400 group-hover:underline text-xl'>{result?.title}</h2>
+                            </a>
+
+                            {/*  */}
+                            <p className='line-clamp-3 text-[15px] mt-2'>{result?.snippet}</p>
+                        </div>
+
+                    ))}
+                </div>
 
                 {/* GPT Chat Part */}
 
